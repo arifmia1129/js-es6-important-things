9ddc3350698b1e913034b06bac18fc2d5bcedaf1
arifvtti <arif.vtti@gmail.com>
2022-03-14 08:42:44 +0600

    problem-2

--- script.js
+++ script.js
@@ -1,6 +1,36 @@
-let temparetureNow = 30;
+// Problem-1
+/* let temparetureNow = 30;
 temparetureNow = 35;
 console.log("Let variable example:", temparetureNow);
 
 const wifeName = "Binu";
-console.log("Const variable example:", wifeName);
+console.log("Const variable example:", wifeName); */
+
+// Problem-2
+
+/* const num1 = 25;
+const num2 = 52;
+
+if (num1 > num2) {
+    console.log("Numb-1 is bigger than num-2");
+}
+
+if (num1 < num2) {
+    console.log("Num2 is bigger than num-1");
+}
+
+if (num1 === num2) {
+    console.log("Num1 is equal to num-2");
+}
+
+if (num1 !== num2) {
+    console.log("Num1 is not equal to num-2");
+}
+
+if (num1 >= num2) {
+    console.log("Num-1 is greater than or equal to num-2");
+}
+
+if (num1 <= num2) {
+    console.log("Num-2 is greater than or equal to num-1");
+} */
